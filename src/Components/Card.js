ff516dc8d6d7297900f12808f78592e932bdf57a
Neil Conway <npconway@gmail.com>
2020-02-15 12:59:40 -0500

    changed image for each card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, birth_year, height }) => {
 	return (
 		<div className='tc bg-white-70 dib br3 pa3 ma2 grow'>
-			<img alt='Star Wars Characters' src='https://image.businessinsider.com/5dceb1907eece53e71466e02?width=2500&format=jpeg&auto=webp' height="200" width="200" />
+			<img alt='Star Wars Characters' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png' height="200" width="200" />
 			<div>
 				<h2>{name}</h2>
 				<p>{birth_year}</p>
